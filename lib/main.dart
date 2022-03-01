cd3788b7ac3606826fa33acd4f49c0a5c74e4b65
Akashiutchiha <christianhonore2003@gmail.com>
2022-03-01 18:59:56 +0100

    coool message

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: (AppBar(
-        title: Text("Health Map"),
+        title: Text("HealthMap"),
         leading: IconButton(
           icon: Icon(Icons.menu),
           onPressed: () {},
