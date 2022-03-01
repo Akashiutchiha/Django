c472401574f2df93438e87fed7bb317bbcb15ba4
Akashiutchiha <christianhonore2003@gmail.com>
2022-03-01 18:29:04 +0100

    Health Map This is Application software which has the purpose of helping users to find the nearest medical centre in a town(city).
    To make it simple this is just a start.

---
+++ android/app/src/main/kotlin/com/example/healthmap/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.healthmap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:latlong2/latlong.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: (AppBar(
+        title: Text("Health Map"),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: [Icon(Icons.notifications_none)],
+      )),
+      body: Center(
+        child: Container(
+          child: Column(children: [
+            Flexible(
+              child: FlutterMap(
+                options: MapOptions(
+                  center: LatLng(5.9597, 10.14597),
+                  zoom: 2,
+                ),
+                layers: [
+                  TileLayerOptions(
+                      urlTemplate:
+                          "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      subdomains: ['a', 'b', 'c']),
+                  MarkerLayerOptions(
+                    markers: [
+                      Marker(
+                        width: 20.0,
+                        height: 20.0,
+                        point: LatLng(5.9597, 10.14597),
+                        builder: (ctx) => Icon(Icons.pin_drop_outlined),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
+      floatingActionButton: (IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.explore),
+        iconSize: 80,
+        color: Colors.blue,
+      )),
+    );
+  }
+}
